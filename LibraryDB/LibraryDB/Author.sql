656db8ca186c284ade797db24ef397fd8f12d657
Connor-Dickson <102051563@student.swin.edu.au>
2019-04-02 14:22:24 +1100

    Initial Commit

---
+++ LibraryDB/LibraryDB/Author.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Author]
+(
+	[AID] INT NOT NULL PRIMARY KEY, 
+    [AFName] NCHAR(10) NOT NULL, 
+    [ALName] NCHAR(10) NOT NULL, 
+    [ATFN] NVARCHAR(50) NOT NULL
+)
---
+++ LibraryDB/LibraryDB/Student.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[SID] INT NOT NULL PRIMARY KEY, 
+    [FName] NCHAR(10) NOT NULL, 
+    [LName] NCHAR(10) NOT NULL, 
+    [Email] NVARCHAR(MAX) NOT NULL, 
+    [MobileNo] INT NOT NULL
+)
